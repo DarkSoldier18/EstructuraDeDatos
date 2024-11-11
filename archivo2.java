deb95f796114e023f5f52b9e2e8f9109e7afcd7e
DarkSoldier <jose.chin2002@gmail.com>
2024-11-10 21:16:13 -0400

    añadiendo funcion factorial

--- archivo2.java
+++ archivo2.java
@@ -2,4 +2,12 @@ public class archivo2 {
     public static void main(String[] args) {
         System.out.println("hola");
     }
+
+    public int factorial(int n){
+        if(n<=1){
+            return 1;
+        }else{
+            return n*factorial(n-1);
+        }
+    }
 }
