d5ae896f2090fd21a48d876a15021877025bc67f
DarkSoldier <jose.chin2002@gmail.com>
2024-11-10 20:03:56 -0400

    mostrando status corto

--- archivo1.java
+++ archivo1.java
@@ -2,5 +2,6 @@ public class archivo1 {
 
     public static void main(String[] args) {
         System.out.println("jose alberto chinchilla vargas");
+        System.out.println("jose chinchilla vargas");
     }
 }
